319b931e120db05616ab7a614b83d8af64252e7c
Lojas Pintos <danielmatos@lojaspintos.com.br>
2019-11-03 23:39:52 -0300

    subindo dependencias - deploy 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,10 @@
 
 set :application_name, 'pincred'
 #set :domain, 'pincred.com.br'
-set :deploy_to, '/var/www/pincred'
 set :repository, 'git://git@github.com/daniel8486/pincred.git'
 #set :branch, 'master'
+
+set :deploy_to, '/var/www/pincred'
 
 append :linked_files, "config/database.yml", "config/storage.yml", "config/master.key"
 append :linked_dirs, "log", "tmp"
@@ -21,40 +22,18 @@ set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
  
 set :rvm_ruby_version, '2.6.1'
 
-# Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
-#   set :port, '30000'           # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
-
-# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
-# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
-# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
-task :remote_environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-    #invoke :'rvm:use', 'ruby-2.6.1-p125@default'
-end
-
 namespace :puma do
-desc 'Create Puma dirs'
-task :create_dirs do
-  on roles(:app) do
+ desc 'Create Puma dirs'
+   task :create_dirs do
+   on roles(:app) do
     execute "mkdir #{shared_path}/tmp/sockets -p"
     execute "mkdir #{shared_path}/tmp/pids -p"
   end
 end
 
-desc "Restart Nginx"
-task :nginx_restart do
-  on roles(:app) do
+ desc "Restart Nginx"
+   task :nginx_restart do
+   on roles(:app) do
     execute "sudo service nginx restart"
   end
 end
@@ -62,40 +41,4 @@ end
 before :start, :create_dirs
 after :start, :nginx_restart
 end
-
-# Put any custom commands you need to run at setup
-# All paths in `shared_dirs` and `shared_paths` will be created on their own.
-task :setup do
-  # command %{rbenv install 2.3.0 --skip-existing}
-end
-
-desc "Deploys the current version to the server."
-task :deploy do
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :'git:ensure_pushed'
-  deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
-    invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-    invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
-    invoke :'deploy:cleanup'
-
-    on :launch do
-      in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
-      end
-    end
-  end
-
-  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
-  # run(:local){ say 'done' }
-end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - https://github.com/mina-deploy/mina/tree/master/docs
 
