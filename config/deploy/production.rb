6d807b08d4f540f6f9debe7b8067489e3ff1143f
Lojas Pintos <danielmatos@lojaspintos.com.br>
2019-11-05 10:43:46 -0300

    consertando deploy

--- config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-set :branch, 'master'
-set :server_address, '167.172.42.75'
- 
-ask(:password, nil, echo: false)
-server fetch(:server_address), user: "deploy", roles: %w{app db web}
- 
-set :nginx_server_name, fetch(:server_address)
-set :puma_preload_app, true
-
