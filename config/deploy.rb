7c823071001a530e784c2fc81273b35b5a51b349
Lojas Pintos <danielmatos@lojaspintos.com.br>
2019-11-05 11:19:46 -0300

    atualizacao capistrano - ajustes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "pin_cred_prod"
+set :repo_url, "git@github.com:daniel8486/pincred.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/pin_cred_prod"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,10 +21,10 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+ append :linked_files, "config/database.yml" , "config/storage.yml" , "config/master.key"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+ append :linked_dirs, "log", "tmp"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,7 +33,39 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+ set :keep_releases, 5
+ 
+ set :migration_role, :app
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+ 
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+ 
+set :nginx_sites_available_path, "/etc/nginx/sites-available"
+set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
+ 
+set :rvm_ruby_version, '2.6.1'
+
+namespace :puma do
+    desc 'Create Puma dirs'
+    task :create_dirs do
+      on roles(:app) do
+        execute "mkdir #{shared_path}/tmp/sockets -p"
+        execute "mkdir #{shared_path}/tmp/pids -p"
+      end
+    end
+   
+    desc "Restart Nginx"
+    task :nginx_restart do
+      on roles(:app) do
+        execute "sudo service nginx restart"
+      end
+    end
+   
+    before :start, :create_dirs
+    after :start, :nginx_restart
+  end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,12 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+set :branch, 'master'
+set :server_address, '167.172.42.75'
+ 
+ask(:password, nil, echo: false)
+server fetch(:server_address), user: "deploy", roles: %w{app db web}
+ 
+set :nginx_server_name, fetch(:server_address)
+set :puma_preload_app, true
