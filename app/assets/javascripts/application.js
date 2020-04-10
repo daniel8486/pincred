0a43e59102e4eb8b596629f0211ea9b3fe6ef425
Lojas Pintos <danielmatos@lojaspintos.com.br>
2020-04-10 15:14:51 -0300

    corrigindo o erro do uglefi

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery
-//= require materialize-sprockets
 //= require sweetalert
 //= require bootstrap/dist/js/bootstrap 
 //= require popper.js/dist/popper
